5ddc99f2d8afb4380c288d6c533f86bf029f564a
Benjamin MARQUES BALULA <marques.benjaminb@gmail.com>
2024-04-24 14:45:43 +0200

    test: added csv tests

--- reading_csv.test.js
+++ reading_csv.test.js
@@ -1,7 +1,23 @@
-test("File is read correctly", () => {
-    const data = readCsv("data.csv")
+const { readCsv } = require(".csv_manipulations.js");
+describe("Manipulating CSV", () => {
+    test("File is read correctly", () => {
+        const data = readCsv("data.csv")
 
-    expect(data).toBeArray()
-    expect(data.length).toBe(5)
-    expect(data[0]).toBeNumber()
+        expect(Array.isArray(data)).toBeTruthy()
+        expect(data.length).toBe(5)
+        expect(data.every(e => Number.isInteger(e))).toBeTruthy()
+    });
+
+    test("Data are added correctly in CSV", () => {
+        const data = readCsv("data.csv")
+        const newData = [1, 2, 3, 4, 5]
+        const newDataLength = data.length + newData.length
+
+        addDataToCsv("data.csv", newData)
+
+        const newDataFromCsv = readCsv("data.csv")
+
+        expect(newDataFromCsv.length).toBe(newDataLength)
+        expect(newDataFromCsv).toEqual([...data, ...newData])
+    });
 });
