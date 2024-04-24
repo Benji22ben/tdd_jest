321344bdf685ef33aa5f33b96f6e8aabe4d14829
Benjamin MARQUES BALULA <marques.benjaminb@gmail.com>
2024-04-24 14:46:05 +0200

    functions: added csv manipulations functions

---
+++ csv_manipulations.js
@@ -0,0 +1,10 @@
+function readCsv() {
+    // Read data.csv file
+    // Return array of data
+}
+
+function addDataToCsv() {
+    // Add data to data.csv file
+}
+
+module.exports = { readCsv, addDataToCsv };
